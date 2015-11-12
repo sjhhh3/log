d32b80abd4d1cf62d6fc4a3b1c0271890857c755
sjhhh3 <sjhhh33@gmail.com>
2015-11-12 18:33:22 -0500

    Update connect.php

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,5 +1,5 @@
 <?php
 $connect_error = '<strong>Sorry, we\'re experiencing SQL connection problems.</strong>';
-mysql_connect("mysql56.rdsmboxt15r0pxc.rds.bj.baidubce.com","sjhhh3","sjh1993211") or die($connect_error);
+mysql_connect("us-cdbr-azure-east-a.cloudapp.net","b0eaced6e338d1","dc0f09d6") or die($connect_error);
 mysql_select_db('login') or die($connect_error);
 ?>
