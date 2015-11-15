adb2803b62ed805ac6bcc098eebb8b95b33fa0c3
sjhhh3 <sjhhh33@gmail.com>
2015-11-15 18:50:08 -0500

    Update users.php

--- core/functions/users.php
+++ core/functions/users.php
@@ -14,8 +14,17 @@ function user_data($user_id){
 	$user_id = (int)$user_id;
 	
 	$func_num_args = func_num_args();
-	echo $func_num_args;
-};
+	$func_get_args = func_get_args();
+	
+	if ($func_num_args > 1) {
+		unset($func_get_args[0]);
+		
+		$fields = '`' . implode('`, `', $func_get_args) . '`';
+		$data = mysql_fetch_assoc(mysql_query("SELECT $fields FROM `users` WHERE `user_id` = $user_id"));
+
+		return $data;
+	}
+}
 	
 
 function logged_in(){
