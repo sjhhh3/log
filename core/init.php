556f9be59cc78af62154f76d738b34e077f16224
sjhhh3 <sjhhh33@gmail.com>
2015-11-15 16:58:07 -0500

    Update init.php

--- core/init.php
+++ core/init.php
@@ -5,5 +5,9 @@ require 'database/connect.php';
 require 'functions/general.php';
 require 'functions/users.php';
 
+if (logged_in() === ture) {
+  $session_user_id = $_SESSION['user_id'];
+  $user_data = user_data($session_user_id, 'user_id', 'username', 'password', 'firstname', 'lastname', 'email', 'acive', 'profile');
+}
 //$errors = array();
 ?>
