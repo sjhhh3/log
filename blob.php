73fa0b95b46912c2677b03f439ae42fe49ab14d8
sjhhh3 <sjhhh33@gmail.com>
2015-11-14 19:56:13 -0500

    Update blob.php

--- blob.php
+++ blob.php
@@ -1,30 +1,21 @@
 <?php
 require_once 'vendor\autoload.php';
-echo "0";
 use WindowsAzure\Common\ServicesBuilder;
 try {
 
-$connectionString = 'DefaultEndpointsProtocol=https;AccountName=datab;AccountKey=Gr+c6yEphuffTGSg1tVIKnOchH931afSEmIWoHhWt1aIFPUD7brmTNd2dzNJAWgIZaAa5BTWcXtAjjVwB9wVVQ==';
+$connectionString = 'DefaultEndpointsProtocol=https;AccountName=account name;AccountKey=account key';
 echo "1";
 
 $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
 echo "2";
 
 
-
-$content = fopen("c:\HaxLogs.txt", "r");
-$blob_name = "myblob";
-
-try {
-    //Upload blob
-    $blobRestProxy->createBlockBlob("pic", $blob_name, $content);
-}
-catch(ServiceException $e){
-    // Handle exception based on error codes and messages.
-    // Error codes and messages are here:
-    // http://msdn.microsoft.com/library/azure/dd179439.aspx
-    $code = $e->getCode();
-    $error_message = $e->getMessage();
-    echo $code.": ".$error_message."<br />";
+} catch(ServiceException $e){
+  // Handle exception based on error codes and messages.
+  // Error codes and messages are here: 
+  // http://msdn.microsoft.com/en-us/library/windowsazure/dd179446.aspx
+  $code = $e->getCode();
+  $error_message = $e->getMessage();
+  echo $code.": ".$error_message."<br />";
 }
 ?>
