c84aa105fc001ec18fa13093b5bbd0ac9e31276d
sjhhh3 <sjhhh33@gmail.com>
2015-11-14 19:03:09 -0500

    Update blob.php

--- blob.php
+++ blob.php
@@ -3,12 +3,12 @@ require_once 'vendor\autoload.php';
 
 use WindowsAzure\Common\ServicesBuilder;
 use WindowsAzure\Common\ServiceException;
-
+$connectionString = 'DefaultEndpointsProtocol=http;AccountName=datab;AccountKey=Gr+c6yEphuffTGSg1tVIKnOchH931afSEmIWoHhWt1aIFPUD7brmTNd2dzNJAWgIZaAa5BTWcXtAjjVwB9wVVQ==';
 // Create blob REST proxy.
 $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
 
 
-$content = fopen("HaxLogs.txt", "r");
+$content = fopen("C:\HaxLogs.txt", "r");
 $blob_name = "myblob";
 
 try {
