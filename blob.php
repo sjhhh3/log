2dfd4e193d90e9e54906357f7b14812cf69bbaee
sjhhh3 <sjhhh33@gmail.com>
2015-11-15 17:23:32 -0500

    Update blob.php

--- blob.php
+++ blob.php
@@ -1,30 +1,11 @@
-<?php
-echo"1";
-require_once 'vendor\autoload.php';
 
-use WindowsAzure\Common\ServicesBuilder;
-use WindowsAzure\Common\ServiceException;
-echo"2";
-// Create blob REST proxy.
-$connectionString = 'DefaultEndpointsProtocol=https;AccountName=datab;AccountKey=Gr+c6yEphuffTGSg1tVIKnOchH931afSEmIWoHhWt1aIFPUD7brmTNd2dzNJAWgIZaAa5BTWcXtAjjVwB9wVVQ==';
-$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
-echo"3";
 
-$content = file_get_contents("upload/1.jpg");
-$blob_name = "myblob1";
-echo"4";
-
-try {
-    //Upload blob
-    $blobRestProxy->createBlockBlob("pic", $blob_name, $content);
+<?php 
+include 'core/init.php';
+if (logged_in() === false){
+	session_destroy();
+	header('Location: index.php');
+	exit();
 }
 
-catch(ServiceException $e){
-    // Handle exception based on error codes and messages.
-    // Error codes and messages are here:
-    // http://msdn.microsoft.com/library/azure/dd179439.aspx
-    $code = $e->getCode();
-    $error_message = $e->getMessage();
-    echo $code.": ".$error_message."<br />";
-}
 ?>
