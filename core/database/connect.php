fc33f533c9f179204993ec41815c42687c27bdeb
sjhhh3 <sjhhh33@gmail.com>
2015-11-15 23:10:33 -0500

    Update connect.php

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,6 +1,6 @@
 <?php
 $connect_error = '<strong>Sorry, we\'re experiencing SSQL connection problems.</strong>';
 $connect_error2 = '<strong>Sorry, we\'re experiencing DB connection problems.</strong>';
-mysql_connect('mysql56.rdsmz5f4ywl100h.rds.bj.baidubce.com','sjhhh3','sjh1993211') or die($connect_error);
+mysql_connect("mysql56.rdsmz5f4ywl100h.rds.bj.baidubce.com","sjhhh3","sjh1993211") or die($connect_error);
 mysql_select_db('login') or die($connect_error2);
 ?>
