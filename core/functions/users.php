42b2bfa7c8f54f08aef5f595d2e6a329330512bc
sjhhh3 <sjhhh33@gmail.com>
2015-11-16 00:31:59 -0500

    Update users.php

--- core/functions/users.php
+++ core/functions/users.php
@@ -3,7 +3,7 @@ function change_profile_image($user_id, $temp_name, $extension){
 	$file_path = '/images/' .substr(md5(time()), 0, 10) . '.' . $extension;
 	move_uploaded_file($file_name, $file_path);
 	echo $file_path;
-	mysql_query("UPDATE `users` SET `profile` = " . $file_path . " WHERE `user_id` = " . (int)$user_id);
+	mysql_query("UPDATE `users` SET `profile` = '" . $file_path . "' WHERE `user_id` = " . (int)$user_id);
 }
 function register_user($register_data){
 	array_walk($register_data, 'array_sanitize');
