0a99f2cecf83b7d51ef4cc9fe381830c769dd0cd
sjhhh3 <sjhhh33@gmail.com>
2015-11-16 15:41:09 -0500

    Update main.php

--- main.php
+++ main.php
@@ -39,7 +39,7 @@ if (isset($name)){
 	
 	change_profile_image($session_user_id, $tmp_name, $extension);
 	
-	if (move_uploaded_file($tmp_name, $file_path)){
+	if (move_uploaded_file($tmp_name, $location.$name)){
 		echo "File Have Been Uploaded.";
 	}else{
 		echo 'There was an error.';
