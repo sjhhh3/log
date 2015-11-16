db9c47f943254d6493083a6edf0812bac463b00e
sjhhh3 <sjhhh33@gmail.com>
2015-11-15 23:35:48 -0500

    Update connect.php

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,5 +1,6 @@
 <?php
-$connect_error = '<strong>Sorry, we\'re experiencing SQL connection problems.</strong>';
-mysql_connect("us-cdbr-azure-east-a.cloudapp.net","b0eaced6e338d1","8881818068fc292") or die($connect_error);
-mysql_select_db('civile') or die($connect_error);
+$connect_error = '<strong>Sorry, we\'re experiencing SSQL connection problems.</strong>';
+$connect_error2 = '<strong>Sorry, we\'re experiencing DB connection problems.</strong>';
+mysql_connect("civile.rdsmz5f4ywl100h.rds.bj.baidubce.com","sjhhh3","sjh1993211") or die($connect_error);
+mysql_select_db('login') or die($connect_error2);
 ?>
