6a4d27f2a2ed0ae280217a6c9fb86756434a8c7a
sjhhh3 <sjhhh33@gmail.com>
2015-11-16 00:36:29 -0500

    Update connect.php

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,8 +1,8 @@
 <?php
 $connect_error = '<strong>Sorry, we\'re experiencing SSQL connection problems.</strong>';
 $connect_error2 = '<strong>Sorry, we\'re experiencing DB connection problems.</strong>';
-mysql_connect("civile.rdsmz5f4ywl100h.rds.bj.baidubce.com","sjhhh3","sjh1993211") or die($connect_error);
+mysql_connect("us-cdbr-azure-east-a.cloudapp.net","b0eaced6e338d1","8881818068fc292") or die($connect_error);
 mysql_select_db('login') or die($connect_error2);
 ?>
 
-// us-cdbr-azure-east-a.cloudapp.net","b0eaced6e338d1","8881818068fc292
+// civile.rdsmz5f4ywl100h.rds.bj.baidubce.com","sjhhh3","sjh1993211
