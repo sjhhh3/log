d2a9a9674eb2d15acb799f7991999f2f21d072da
sjhhh3 <sjhhh33@gmail.com>
2015-11-16 20:36:30 -0500

    Update pic.php

--- pic.php
+++ pic.php
@@ -27,6 +27,31 @@
 			?>
 
         <br><br><br><h1>Please Confirm Your Image And Click To Upload To The Cloud.</h1>
+        
+        <?php
+        require_once 'vendor\autoload.php';
+        use WindowsAzure\Common\ServicesBuilder;
+        use WindowsAzure\Common\ServiceException;
+// Create blob REST proxy.
+$connectionString = 'DefaultEndpointsProtocol=https;AccountName=datab;AccountKey=Gr+c6yEphuffTGSg1tVIKnOchH931afSEmIWoHhWt1aIFPUD7brmTNd2dzNJAWgIZaAa5BTWcXtAjjVwB9wVVQ==';
+$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+$content = file_get_contents("$dir");
+$blob_name = "$dir";
+try {
+    //Upload blob
+    $blobRestProxy->createBlockBlob("pic", $blob_name, $content);
+}
+catch(ServiceException $e){
+    // Handle exception based on error codes and messages.
+    // Error codes and messages are here:
+    // http://msdn.microsoft.com/library/azure/dd179439.aspx
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo $code.": ".$error_message."<br />";
+}
+        ?>
+        
+        
         <script src="assets/js/jquery-1.8.2.min.js"></script>
         <script src="assets/js/supersized.3.2.7.min.js"></script>
         <script src="assets/js/supersized-init.js"></script>
