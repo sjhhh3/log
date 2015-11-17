a3e433cfcb3064c00244b23e353d8b0865594178
sjhhh3 <sjhhh33@gmail.com>
2015-11-16 20:31:54 -0500

    Update pic.php

--- pic.php
+++ pic.php
@@ -26,7 +26,7 @@
             echo '<img src="'.$img.'" alt="图片" />';
 			?>
 
-        <h1>Please Confirm Your Image And Click To Upload To The Cloud.</h1>
+        <br><br><br><h1>Please Confirm Your Image And Click To Upload To The Cloud.</h1>
         <script src="assets/js/jquery-1.8.2.min.js"></script>
         <script src="assets/js/supersized.3.2.7.min.js"></script>
         <script src="assets/js/supersized-init.js"></script>
