af0af7ce55ddcb8117d2c9cfd92d9cff88684079
sjhhh3 <sjhhh33@gmail.com>
2015-11-17 16:41:48 -0500

    Update down.php

--- down.php
+++ down.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['file_name'])){
 	//$file = $_POST['file_name'];
-	$file = "f9f20a7838.jpg";
+	$file = "55e7e83c09.jpg";
 	header('Content-Type: image/jpeg');
     header('Content-Disposition:  attachment;  filename= "'.$file.'"'); 
     readfile('upload/'.$file);
