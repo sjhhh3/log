102078281c0da3b4fe2687c32c99e509acf13298
sjhhh3 <sjhhh33@gmail.com>
2015-11-17 16:53:56 -0500

    Update pic.php

--- pic.php
+++ pic.php
@@ -19,12 +19,13 @@
 
 			
 			<?php
+			include 'core/init.php';
 			if (logged_in() === false){
 	session_destroy();
 	header('Location: index.php');
 	exit();
 }
-			include 'core/init.php';
+			
 			$dir = $user_data['profile'];
 			$mname = explode("/", $dir);
 			$img = "$dir";
